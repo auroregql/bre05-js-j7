9276c74e5290f73adb554143c4922d890fb441b2
auroregql <aurore.gicquel.06@gmail.com>
2026-01-14 09:40:24 +0000

    Exercice 3 terminé : Transformer un objet en JSON

---
+++ json-storage/exercice3/index.js
@@ -0,0 +1,18 @@
+let data = {
+    firstName : "Harry",
+    lastName : "Bow",
+    motto : "C'est beau la vie !"
+};
+
+let str = "{\"firstName\":\"Colonel\",\"lastName\":\"Sanders\",\"motto\":\"Il est bon mon poulet\"}";
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    JSON.stringify(data);
+    
+    console.log(data);
+    
+    JSON.parse(str);
+    
+    console.log(str);
+});
