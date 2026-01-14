b1c65f7c49940c60a66f91ea7c1c4040e5cadf3f
auroregql <aurore.gicquel.06@gmail.com>
2026-01-14 15:45:35 +0000

    Exercice 4 corrigé : stocker du JSON dans le storage

--- json-storage/exercice4/index.js
+++ json-storage/exercice4/index.js
@@ -1,14 +1,22 @@
+// creation de l'objet
+
 let user = {
   firstName: "Harry",
   lastName: "Bow",
   motto: "C'est beau la vie !"
 };
 
+// chargement du DOM
 document.addEventListener("DOMContentLoaded", function () {
+ 
+//Récupération des données de l'objet 
   let data = sessionStorage.getItem("user");
 
+//Si les données sont inexistantes mettre Objet en JSON
   if (data === null) {
     sessionStorage.setItem("user", JSON.stringify(user));
+    
+// Sinon mettre l'objet en variable Javascript
   } else {
     user = JSON.parse(data);
     console.log(user);
