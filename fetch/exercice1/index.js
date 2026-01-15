eb71b7393926d7457f64376b4a8712dd41a4abc6
auroregql <aurore.gicquel.06@gmail.com>
2026-01-15 09:57:28 +0000

    Exercice 1 terminé : récupérer les données d'un utilisateur

---
+++ fetch/exercice1/index.js
@@ -0,0 +1,9 @@
+let url = "https://corsproxy.io/?" + encodeURIComponent(
+    "https://maridoucet.sites.3wa.io/user-api/user/15"
+);
+
+fetch(url)
+
+    .then(response => response.json()) 
+    .then(data => console.log(data)) 
+    .catch(err => console.error(err));
