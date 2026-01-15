a8587444e3a5c186ee31481e2e8686da73d6db09
auroregql <aurore.gicquel.06@gmail.com>
2026-01-15 14:29:42 +0000

    Exercice 4 corrigé : générer du HTML avec les infos récupérées avec fetch

--- fetch/exercice4/index.js
+++ fetch/exercice4/index.js
@@ -10,19 +10,19 @@ window.fetch(url)
     
     //Création du h1, du texte dans le h1, puis de le positionner dans le body
     let h1 = document.createElement('h1');
-    let h1Text = document.createTextNode("username");
+    let h1Text = document.createTextNode(user.data.username);
     h1.appendChild(h1Text);
     document.body.appendChild(h1);
     
     //Création du h2, du texte dans le h2, puis de le positionner dans le body
     let h2 = document.createElement('h2');
-    let h2Text = document.createTextNode("firstname", "lastname");
+    let h2Text = document.createTextNode(user.data.firstName + user.data.lastName);
     h2.appendChild(h2Text);
     document.body.appendChild(h2);
     
     //Création du h3, du texte dans le h3, puis de le positionner dans le body
     let h3 = document.createElement('h3');
-    let h3Text = document.createTextNode("email");
+    let h3Text = document.createTextNode(user.data.email);
     h3.appendChild(h3Text);
     document.body.appendChild(h3);
 
